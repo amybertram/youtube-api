fe6e374e72252327ff5c18cfe7525db1ca9fd7ad
Amy Bertram <amy.bertram1@gmail.com>
2017-11-25 13:01:24 -0800

    Next page search functionality

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let state = {
+//Single State Object
+var state = {
   nextPageToken:"",
   query:""
 }
@@ -28,49 +29,29 @@ function displaySearchResults(data){
 
 //Render Functions
 function renderResult(result) {
-  
   var resultHTML =  
-    `<div class="result-section" data-video-id="${result.id.videoId}">
-       <h2>${result.snippet.title}</h2>
-       <h3>by <a href="https://www.youtube.com/channel/${result.snippet.channelId}" target="_blank">${result.snippet.channelTitle}</a></h3>
-       <img src="${result.snippet.thumbnails.medium.url}">
-    </div>`;
+    `<div class="result-section">` +
+      `<h2>${result.snippet.title}</h2>` +
+      `<h3>by <a href="https://www.youtube.com/channel/${result.snippet.channelId}" target="_blank">${result.snippet.channelTitle}</a></h3>` + 
+      `<a href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank"><img src="${result.snippet.thumbnails.medium.url}"></a>` +
+    `</div>`;
   return resultHTML; 
 }
-//<a href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank">
 
 //Event Listeners
 function watchSubmit() {
   $('.js-search-form').submit(function(event) {
     event.preventDefault();
-    let queryTarget = $('#js-query');
-    queryTarget.val();
-    queryTarget.val("");// clear out the input
-    $('#next-page-button').removeClass("hidden")
+    var queryTarget = $('#js-query');
+    state.query = queryTarget.val();
+    // clear out the input
+    queryTarget.val("");
+    $('#next-page-button').removeClass("hidden");
     getDataFromApi(state.query, displaySearchResults);
   });
-  
-  $('#next-page-button').click(function(event) {
+   $('#next-page-button').click(function(event) {
     getDataFromApi(state.query, displaySearchResults);
     $(window).scrollTop(0);
-  });
-  
-  $(".js-search-results").on("click", '.result-section',function(event) {
-    $('#black-screen').removeClass("hidden")
-    
-    var videoId = $(this).attr("data-video-id")
-    console.log(videoId)
-    $("#white-screen").html(`
-      <iframe 
-        width="560" 
-        height="315" 
-        src="https://www.youtube.com/embed/${videoId}" 
-        frameborder="0" 
-        allowfullscreen>
-      </iframe>`)
-  });
-  $("#black-screen").click(function(event) {
-    $('#black-screen').addClass("hidden")
   });
 }
 
